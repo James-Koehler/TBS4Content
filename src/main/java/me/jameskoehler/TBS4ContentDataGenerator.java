03540156c05dd289b41f1e088a1a6711663813a4
James Koehler <jameskoehler@protonmail.com>
2024-07-04 18:17:18 -0400

    Added template mod files

---
+++ src/main/java/me/jameskoehler/TBS4ContentDataGenerator.java
@@ -0,0 +1,11 @@
+package me.jameskoehler;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TBS4ContentDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

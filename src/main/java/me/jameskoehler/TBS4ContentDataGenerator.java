835309e4178506bfcb4c3522b4bb42f43cad950f
James Koehler <jameskoehler@protonmail.com>
2024-07-04 19:03:27 -0400

    fixed the datagen

--- src/main/java/me/jameskoehler/TBS4ContentDataGenerator.java
+++ src/main/java/me/jameskoehler/TBS4ContentDataGenerator.java
@@ -2,14 +2,32 @@ package me.jameskoehler;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.registry.RegistryWrapper;
 
-
+import java.util.concurrent.CompletableFuture;
 
 public class TBS4ContentDataGenerator implements DataGeneratorEntrypoint {
+
+	private static class TBS4RecipeGenerator extends FabricRecipeProvider{
+
+		private TBS4RecipeGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+			super(output, registriesFuture);
+		}
+
+		@Override
+		public void generate(RecipeExporter exporter) {
+
+		}
+	}
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(TBS4RecipeGenerator::new);
 	}
 }

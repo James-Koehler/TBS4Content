a69932f0991eb6c2b85496bb46dab6d54d703fc3
Concavebark <concavebarkplaysgames@yahoo.com>
2024-07-04 20:57:31 -0400

    Added Return Rod and it's Functionality

---
+++ src/main/java/me/jameskoehler/ReturnRod.java
@@ -0,0 +1,37 @@
+package me.jameskoehler;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.apache.logging.log4j.core.jmx.Server;
+import org.slf4j.LoggerFactory;
+
+public class ReturnRod extends Item {
+    public ReturnRod(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (!world.isClient() && user instanceof ServerPlayerEntity serverUser) {
+            //on server
+            if (world.getRegistryKey().equals(serverUser.getSpawnPointDimension())) {
+                BlockPos spawnPoint = serverUser.getSpawnPointPosition();
+                LoggerFactory.getLogger("tbs4content").info(spawnPoint.toString());
+
+                user.requestTeleport(spawnPoint.getX()+0.5, spawnPoint.getY()+0.7, spawnPoint.getZ()+0.5);
+                user.playSound(SoundEvents.ENTITY_CREEPER_PRIMED);
+                return TypedActionResult.success(user.getStackInHand(hand));
+            }
+        }
+        return TypedActionResult.success(user.getStackInHand(hand));
+    }
+
+}
--- src/main/java/me/jameskoehler/TBS4Content.java
+++ src/main/java/me/jameskoehler/TBS4Content.java
@@ -2,6 +2,11 @@ package me.jameskoehler;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +15,9 @@ public class TBS4Content implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("tbs4content");
+	public static final String modID = "tbs4content";
+
+	public static final ReturnRod RETURN_ROD = Registry.register(Registries.ITEM, Identifier.of(modID, "returnrod"), new ReturnRod(new Item.Settings()));
 
 	@Override
 	public void onInitialize() {

83b22e1690f2d11543a92fb622564bf0fafc5a8b
James Koehler <jameskoehler@protonmail.com>
2024-07-04 20:08:31 -0400

    Added some crafting items + recipes.

--- src/main/java/me/jameskoehler/TBS4Content.java
+++ src/main/java/me/jameskoehler/TBS4Content.java
@@ -2,6 +2,10 @@ package me.jameskoehler;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +14,13 @@ public class TBS4Content implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("tbs4content");
+	public static final String modID = "tbs4content";
+
+	// Items
+	public static final Item END_STONE_DUST = Registry.register(Registries.ITEM, Identifier.of(modID, "endstonedust"), new Item(new Item.Settings()));
+	public static final Item END_STONE_CLAY = Registry.register(Registries.ITEM, Identifier.of(modID, "endstoneclay"), new Item(new Item.Settings()));
+	public static final Item COATED_EYE = Registry.register(Registries.ITEM, Identifier.of(modID, "coatedeye"), new Item(new Item.Settings()));
+	public static final Item CERAMIC_EYE = Registry.register(Registries.ITEM, Identifier.of(modID, "ceramiceye"), new Item(new Item.Settings()));
 
 	@Override
 	public void onInitialize() {
--- src/main/java/me/jameskoehler/TBS4ContentDataGenerator.java
+++ src/main/java/me/jameskoehler/TBS4ContentDataGenerator.java
@@ -5,9 +5,16 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.RecipeProvider;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+
+import static me.jameskoehler.TBS4Content.*;
 
 public class TBS4ContentDataGenerator implements DataGeneratorEntrypoint {
 
@@ -20,6 +27,22 @@ public class TBS4ContentDataGenerator implements DataGeneratorEntrypoint {
 		@Override
 		public void generate(RecipeExporter exporter) {
 
+			ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, END_STONE_DUST, 3).input(Items.END_STONE)
+					.criterion(FabricRecipeProvider.hasItem(END_STONE_DUST), FabricRecipeProvider.conditionsFromItem(END_STONE_DUST))
+					.criterion(FabricRecipeProvider.hasItem(Items.END_STONE), FabricRecipeProvider.conditionsFromItem(Items.END_STONE))
+					.offerTo(exporter);
+			ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, END_STONE_CLAY).input(END_STONE_DUST).input(Items.WATER_BUCKET)
+					.criterion(FabricRecipeProvider.hasItem(END_STONE_CLAY), FabricRecipeProvider.conditionsFromItem(END_STONE_CLAY))
+					.criterion(FabricRecipeProvider.hasItem(END_STONE_DUST), FabricRecipeProvider.conditionsFromItem(END_STONE_DUST))
+					.criterion(FabricRecipeProvider.hasItem(Items.WATER_BUCKET), FabricRecipeProvider.conditionsFromItem(Items.WATER_BUCKET))
+					.offerTo(exporter);
+			ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, COATED_EYE).input(END_STONE_CLAY).input(Items.ENDER_EYE)
+					.criterion(FabricRecipeProvider.hasItem(COATED_EYE), FabricRecipeProvider.conditionsFromItem(COATED_EYE))
+					.criterion(FabricRecipeProvider.hasItem(END_STONE_CLAY), FabricRecipeProvider.conditionsFromItem(END_STONE_CLAY))
+					.criterion(FabricRecipeProvider.hasItem(Items.ENDER_EYE), FabricRecipeProvider.conditionsFromItem(Items.ENDER_EYE))
+					.offerTo(exporter);
+
+			RecipeProvider.offerSmelting(exporter, List.of(COATED_EYE), RecipeCategory.MISC, CERAMIC_EYE, 1.5f, 500, "TBS4");
 		}
 	}
 

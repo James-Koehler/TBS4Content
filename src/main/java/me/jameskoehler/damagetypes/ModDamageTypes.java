3c614202da09ad11d249b271bd17465fa97ab810
Concavebark <concavebarkplaysgames@yahoo.com>
2024-07-09 22:29:49 -0400

    Added leukemia_damage_type for custom death message when 1/32k chance is rolled for instant death while player has leukemia, leukemia fully implemented

--- src/main/java/me/jameskoehler/damagetypes/ModDamageTypes.java
+++ src/main/java/me/jameskoehler/damagetypes/ModDamageTypes.java
@@ -11,6 +11,7 @@ import net.minecraft.world.World;
 public class ModDamageTypes {
 
     public static final RegistryKey<DamageType> RADIATION_POISONING_DAMAGE_TYPE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(TBS4Content.modID, "radiation_poisoning"));
+    public static final RegistryKey<DamageType> LEUKEMIA_DAMAGE_TYPE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Identifier.of(TBS4Content.modID, "leukemia"));
 
     public static DamageSource of(World world, RegistryKey<DamageType> key) {
         return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key));
--- src/main/java/me/jameskoehler/event/PlayerTickHandler.java
+++ src/main/java/me/jameskoehler/event/PlayerTickHandler.java
@@ -2,7 +2,10 @@ package me.jameskoehler.event;
 
 import me.jameskoehler.IEntityDataSaver;
 import me.jameskoehler.RadiationData;
+import me.jameskoehler.TBS4Content;
+import me.jameskoehler.damagetypes.ModDamageTypes;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
@@ -14,10 +17,21 @@ public class PlayerTickHandler implements ServerTickEvents.StartTick{
     @Override
     public void onStartTick(MinecraftServer server) {
         for(ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+            IEntityDataSaver dataPlayer = ((IEntityDataSaver) player);
             if (new Random().nextFloat() <= 0.005f) {
-                IEntityDataSaver dataPlayer = ((IEntityDataSaver) player);
                 RadiationData.removeExposure(dataPlayer, 20);
-                player.sendMessage(Text.literal("exposure level: " + dataPlayer.getPersistentData()));
+            }
+            if (new Random().nextFloat() <= 0.002f) {
+                int currExpo = dataPlayer.getPersistentData().getInt("exposure");
+                if(currExpo > 100 && !player.hasStatusEffect(TBS4Content.LEUKEMIA)) {
+                    player.addStatusEffect(new StatusEffectInstance(TBS4Content.LEUKEMIA, -1, 0, false, false));
+
+                }
+            }
+            if (new Random().nextFloat() <= 0.00003125f) {
+                if (player.hasStatusEffect(TBS4Content.LEUKEMIA)) {
+                    player.damage(ModDamageTypes.of(player.getWorld(), ModDamageTypes.LEUKEMIA_DAMAGE_TYPE), 1000);
+                }
             }
         }
     }
